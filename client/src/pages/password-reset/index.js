07b6b9051232997ef4d5429d61ba27e54d7583e6
zy <gohzy89@gmail.com>
2024-10-29 03:09:32 +0800

    created new pages and added the index file

---
+++ client/src/pages/password-reset/index.js
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { useEffect } from 'react';
+
+const PasswordResetPage = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default PasswordResetPage;
