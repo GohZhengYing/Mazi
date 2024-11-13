5689838ed35da6ab53eec87f50fa55572f646e07
zy <gohzy89@gmail.com>
2024-11-13 17:54:22 +0800

    added login page as well

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,12 +1,13 @@
 import { NextPage } from 'next';
 import { useEffect } from 'react';
-import LoadingPage from '../../components/loading/LoadingPage'
+import LoginPageLayout from '../../layouts/login/LoginPageLayout'
 
 const LoginPage = () => {
 
   return (
-    <>
-    </>
+    <LoginPageLayout>
+
+    </LoginPageLayout>
   );
 };
 
