dd5a6c649b00ca5058b2fc8b1f5c88746efa2c24
wJingYen <79895010+wJingYen@users.noreply.github.com>
2024-11-18 01:37:45 +0800

    merged conflicts and added new tabs

--- client/src/components/home/Contacts.tsx
+++ client/src/components/home/Contacts.tsx
@@ -1,9 +1,9 @@
 import { Box, Input, VStack, HStack, Stack, Separator, Text, Badge } from '@chakra-ui/react';
-import { Avatar } from "../components/ui/avatar"
+import { Avatar } from "@/components/ui/avatar"
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function ChatList() {
+export default function Contacts() {
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedChat, setSelectedChat] = useState(null);
 
@@ -13,16 +13,14 @@ export default function ChatList() {
         { id: 2, name: 'Bob', chat: 'hello' },
         { id: 3, name: 'Carol', chat: 'hello' },
         { id: 4, name: 'Dave', chat: 'hello' },
-        { id: 5, name: 'Alice', chat: 'hello' },
-        { id: 6, name: 'Bob', chat: 'hello' },
+        { id: 5, name: 'Carol', chat: 'hello' },
+        { id: 6, name: 'Dave', chat: 'hello' },
         { id: 7, name: 'Carol', chat: 'hello' },
         { id: 8, name: 'Dave', chat: 'hello' },
-        { id: 9, name: 'Bob', chat: 'hello' },
-        { id: 10, name: 'Carol', chat: 'hello' },
-        { id: 11, name: 'Dave', chat: 'hello' },
-        { id: 12, name: 'Carol', chat: 'hello' },
-        { id: 13, name: 'Dave', chat: 'hello' },
-        { id: 14, name: 'Dave', chat: 'hello' },
+        { id: 9, name: 'Carol', chat: 'hello' },
+        { id: 10, name: 'Dave', chat: 'hello' },
+        { id: 11, name: 'Carol', chat: 'hello' },
+        { id: 12, name: 'Dave', chat: 'hello' },
     ];
 
     // Filter chats based on search term
@@ -40,7 +38,7 @@ export default function ChatList() {
     return (
         <Stack bg="#CDF0FF" h="100vh" alignItems="center" w="20%" gap="0">
             {/* Search Bar */}
-            <Box h="3.7rem" fontSize="1.2rem" fontWeight="bold" textAlign="center" p="1.5rem">Chats</Box>
+            <Box h="3.7rem" fontSize="1.2rem" fontWeight="bold" textAlign="center" paddingTop="1.3rem">Contacts</Box>
             <Input
                 placeholder="Search"
                 bg="#FFFFFF"
@@ -51,7 +49,7 @@ export default function ChatList() {
                 onChange={(e) => setSearchTerm(e.target.value)}
                 borderWidth="0"
                 borderRadius="0.4rem"
-                m="0.5rem"
+                m="3"
                 w="93%"
             />
 <Separator borderColor="white" size="sm" />
@@ -69,17 +67,7 @@ export default function ChatList() {
                             _hover={{ bg: 'gray.300' }}
                         >
                             <Avatar m="2"  alignSelf="flex-start" name={chat.name} size="lg" />
-                            <VStack alignSelf="flex-start" gap="0">
-                                <Text textStyle="lg" fontWeight="medium" mb="0" mt="2" >{chat.name}</Text>
-                                <Text color="#6F6A6A" mt="0" mb="1.5" textStyle="sm">
-                                    {chat.chat}
-                                </Text>
-                            </VStack>
-                            <VStack ml="auto" gap="0" mr="3">
-                                <Text textStyle="sm" color="#6F6A6A" mb="0" mt="2">Fri</Text>
-                                {selectedChat?.id === chat.id ? '':
-                                <Badge mt="1" mb="1.5" bg="#24BDFF" borderRadius="4rem">205</Badge>}
-                            </VStack>
+                                <Text textStyle="lg" fontWeight="medium" m="3" ml="0" >{chat.name}</Text>
                         </HStack>
                         <Separator borderColor="white" size="sm" />
                     </React.Fragment>
